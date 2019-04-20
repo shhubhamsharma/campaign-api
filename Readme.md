c7d8a8e65a529aa4a415541158770a7eb1926228
Shubham Sharma <shubham.ks494@gmail.com>
2019-04-20 12:24:53 +0530

    chnages

---
+++ Readme.md
@@ -0,0 +1,8 @@
+### Dependency installation
+Run `npm install`
+
+### Database Migrations
+Run `node ./node_modules/migrate/bin/migrate up`
+
+##Start Application
+Run `npm run start`
--- cronjobs.js
+++ cronjobs.js
@@ -1,26 +1,70 @@
-var Rules = require('./schema/ruleSchema')
+var Rules = require('./schema/ruleSchema');
+var cpmData = require('./schema/userInfo');
 
-module.exports.executor15 =function (){
-    Rules.find({scheduleid:"1"}).then(async res => {
+module.exports.executor15 = function () {
+    Rules.find({ scheduleid: "1" }).then(async res => {
         console.log("notify user 1");
         await res.forEach(async element => {
-            
+            cpmData.aggregate([{
+                $match: {
+                    campaign: { $in: element.option }
+                }
+            }, {
+                $group: {
+                    _id: "$campaign",
+                    cost: { $sum: "$value" }
+                }
+            }]).then(res => {
+                if (eval(element.condition)) {
+                    console.log("notify")
+                }
+            })
         });
-    }).catch(err =>{
+    }).catch(err => {
+        console.log("handle error");
+    });
+}
+module.exports.executorhourly = function () {
+    Rules.find({ scheduleid: "2" }).then(res => {
+        await res.forEach(async element => {
+            cpmData.aggregate([{
+                $match: {
+                    campaign: { $in: element.option }
+                }
+            }, {
+                $group: {
+                    _id: "$campaign",
+                    cost: { $sum: "$value" }
+                }
+            }]).then(res => {
+                if (eval(element.condition)) {
+                    console.log("notify")
+                }
+            })
+        });
+    }).catch(err => {
 
     });
 }
-module.exports.executorhourly =function (){
-    Rules.find({scheduleid:"2"}).then(res => {
-        console.log("notify user 20");
-    }).catch(err =>{
-
-    });
-}
-module.exports.executordaily =function (){
-    Rules.find({scheduleid:"3"}).then(res => {
-        console.log("notify user");
-    }).catch(err =>{
+module.exports.executordaily = function () {
+    Rules.find({ scheduleid: "3" }).then(res => {
+        await res.forEach(async element => {
+            cpmData.aggregate([{
+                $match: {
+                    campaign: { $in: element.option }
+                }
+            }, {
+                $group: {
+                    _id: "$campaign",
+                    cost: { $sum: "$value" }
+                }
+            }]).then(res => {
+                if (eval(element.condition)) {
+                    console.log("notify")
+                }
+            })
+        });
+    }).catch(err => {
 
     });
 }
